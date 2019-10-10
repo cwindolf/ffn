0a015df3986f2b31d682be32fa5293390844c9ee
Charlie Windolf <cwindolf95@gmail.com>
2019-10-10 13:13:48 -0400

    Fix weight swap op

--- models/swapped_encoder_ffn.py
+++ models/swapped_encoder_ffn.py
@@ -1,9 +1,10 @@
+import numpy as np
 import tensorflow as tf
+from models.convstack_3d_encoder import ConvStack3DEncoder
 from ffn.training.models import convstack_3d
 
 
-class SwappedEncoderFFN(convstack_3d.ConvStack3DFFNModel):
-
+class SwappedEncoderFFNModel(convstack_3d.ConvStack3DFFNModel):
     def __init__(
         self,
         encoder_ckpt,
@@ -11,23 +12,50 @@ class SwappedEncoderFFN(convstack_3d.ConvStack3DFFNModel):
         deltas=None,
         batch_size=None,
         depth=9,
+        layer=3,
     ):
         super().__init__(
-            fov_size=fov_size,
-            deltas=deltas,
-            batch_size=batch_size,
-            depth=depth,
+            fov_size=fov_size, deltas=deltas, batch_size=batch_size, depth=depth
         )
         self.encoder_ckpt = encoder_ckpt
+        self.layer = layer
+        self.fov_size = fov_size
+        self.get_encoder_vars()
 
     def define_tf_graph(self):
         super().define_tf_graph()
 
         # Add op to swap encoder weights from encoder ckpt
         # Caller needs to make sure this is run after initial restore
-        einit_op, einit_fd = tf.contrib.framework.assign_from_checkpoint(
-            self.encoder_ckpt, self.encoder.vars, ignore_missing_vars=True
+        einit_op, einit_fd = tf.contrib.framework.assign_from_values(
+            self.encoder_var_names_to_values
         )
 
         self.init_op = einit_op
         self.init_feed_dict = einit_fd
+
+    def get_encoder_vars(self):
+        '''Load encoder weights and remap their names to ffn naming'''
+        encoder_loading_graph = tf.Graph()
+        with encoder_loading_graph.as_default():
+            encoder = ConvStack3DEncoder(
+                fov_size=self.fov_size,
+                input_seed=np.zeros(
+                    [self.batch_size, *self.fov_size, 1], dtype=np.float32
+                ),
+                batch_size=self.batch_size,
+                depth=self.layer,
+            )
+            encoder.define_tf_graph()
+
+            # Encoder var naming -> FFN var naming
+            names = [
+                v.op.name.replace('encoder', 'seed_update')
+                for v in encoder.vars
+            ]
+
+            with tf.Session() as sess:
+                encoder.saver.restore(sess, self.encoder_ckpt)
+                weights = dict(zip(names, sess.run(encoder.vars)))
+
+        self.encoder_var_names_to_values = weights
