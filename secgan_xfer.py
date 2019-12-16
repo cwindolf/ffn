92056603faf0119f29504984b1d386e728ce3fc6
Charlie Windolf <cwindolf95@gmail.com>
2019-12-16 11:10:13 -0500

    Allow inference in both directions.

--- secgan_xfer.py
+++ secgan_xfer.py
@@ -11,6 +11,8 @@ def secgan_infer(
     ffn_fov_size=33,
     generator_depth=8,
     generator_channels=32,
+    generator_margin=16,
+    nchunks=1,
     F_or_G='F',
 ):
     '''Run the generator F to map an unabeled volume to a labeled-ish volume
@@ -22,6 +24,35 @@ def secgan_infer(
     content_volume = content_volume.astype(np.float32)
     content_volume /= 127.5
     content_volume -= 1.0
+
+    # Make chunks
+    # They need to overlap according to generator_margin
+    # Chunksize is kind of a misnomer.
+    # First and last chunks will have size chunksize + gen_margin,
+    # all others chunksize + 2 * genmargin,
+    # Unless the first and last chunks are the same in which
+    # case they'll just be the whole volume.
+    # And of course there might be an off-by-one on the last slice
+    # to deal with integer division.
+    chunksize = content_volume.shape[0] // nchunks
+    chunksizes = [chunksize] * (nchunks - 1)
+    chunksizes.append(content_volume.shape[0] - sum(chunksizes))
+    chunks = []
+    for i, c in enumerate(chunksizes):
+        if i == 0:
+            lo = 0
+        else:
+            lo = i * c - generator_margin
+
+        if i == nchunks - 1:
+            hi = content_volume.shape[0]
+        else:
+            hi = (i + 1) * c + generator_margin
+
+        assert 0 <= lo <= content_volume.shape[0] - chunksize
+        assert chunksize <= hi <= content_volume.shape[0]
+
+        chunks.append(content_volume[lo:hi])
 
     # Init model
     generator = SECGAN(
@@ -36,35 +67,48 @@ def secgan_infer(
     # TF world
     with tf.Graph().as_default():
         if F_or_G == 'F':
-            generator.define_F_graph(content_volume.shape)
+            generator.define_F_graph(chunks[0].shape)
         elif F_or_G == 'G':
-            generator.define_G_graph(content_volume.shape)
+            generator.define_G_graph(chunks[0].shape)
         else:
             raise ValueError(
                 f'F or G? Like, I only ask because you put "{F_or_G}"...'
             )
 
+        xfer_chunks = []
+
         with tf.Session() as sess:
             sess.run(generator.inf_init_op, feed_dict=generator.inf_init_fd)
 
-            xfer_volume = sess.run(
-                generator.xfer_output,
-                feed_dict={
-                    generator.xfer_input: content_volume[None, ..., None]
-                },
-            )
+            for chunk in chunks:
+                xfer_chunk = sess.run(
+                    generator.xfer_output,
+                    feed_dict={generator.xfer_input: chunk[None, ..., None]},
+                )
+                xfer_chunks.append(xfer_chunk.squeeze())
 
     # Post process
     # For now, let's just leave this centered and float typed.
     # But multiply by 127.5 since that will be divided out later
     # by (my fork of) the FFN inference script, I think, haha.
     # IDK, depends on the inference request.
-    xfer_volume = xfer_volume.squeeze()
-    xfer_volume += 1.0
-    xfer_volume *= 127.5
+    xfer_volume = np.concatenate(xfer_chunks)
+    assert all(
+        np.array(xfer_volume.shape)
+        == (np.array(content_volume.shape) - 2 * generator_margin)
+    )
+    # xfer_volume += 1.0
+    # xfer_volume *= 127.5
 
     # Write output
-    dx.writespec(output_spec, xfer_volume)
+    dx.writespec(
+        output_spec,
+        xfer_volume,
+        h5_attrs={
+            'checkpoint_path': checkpoint_path,
+            'content_volume_spec': content_volume_spec,
+        },
+    )
 
 
 # ---------------------------------------------------------------------
@@ -82,6 +126,8 @@ if __name__ == '__main__':
     flags.DEFINE_integer('ffn_fov_size', 33, '')
     flags.DEFINE_integer('generator_depth', 8, '')
     flags.DEFINE_integer('generator_channels', 32, '')
+    flags.DEFINE_integer('generator_margin', 16, '')
+    flags.DEFINE_integer('nchunks', 1, '')
 
     FLAGS = flags.FLAGS
 
@@ -93,7 +139,9 @@ if __name__ == '__main__':
             ffn_fov_size=FLAGS.ffn_fov_size,
             generator_depth=FLAGS.generator_depth,
             generator_channels=FLAGS.generator_channels,
+            generator_margin=FLAGS.generator_margin,
             F_or_G=FLAGS.F_or_G,
+            nchunks=FLAGS.nchunks,
         )
 
     app.run(main)
