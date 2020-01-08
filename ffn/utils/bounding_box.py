0b81ea9d6d3f40b392b4f7a05fe89eb8db465f95
Charlie Windolf <cwindolf95@gmail.com>
2020-01-08 17:37:02 -0500

    Upstream seems to have forgotten an import?

--- ffn/utils/bounding_box.py
+++ ffn/utils/bounding_box.py
@@ -22,6 +22,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from typing import Optional
+
 from bisect import bisect_right
 import copy
 
