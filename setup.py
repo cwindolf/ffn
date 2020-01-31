eb425d989979a8ea85eeba5102efcf86bf24652b
Charlie Windolf <cwindolf95@gmail.com>
2020-01-31 16:27:02 -0500

    PIL=>pillow.

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
-#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ModuleNotFoundError:
+    from distutils.core import setup
 
-from distutils.core import setup
 
 setup(
-	name		= 'ffn',
-	version		= '0.1.0',
-	author		= 'Michal Januszewski',
-	author_email	= 'mjanusz@google.com',
-	packages	= ['ffn', 'ffn.inference', 'ffn.training', 'ffn.utils'],
-	scripts		= ['build_coordinates.py', 'compute_partitions.py', 'run_inference.py', 'train.py'],
-	url		= 'https://github.com/google/ffn',
-	license		= 'LICENSE',
-	description	= 'Flood-Filling Networks for volumetric instance segmentation',
-	long_description= open('README.md').read(),
-	install_requires= ['scikit-image', 'scipy', 'numpy', 'tensorflow', 'h5py', 'PIL', 'absl-py'],
+    name='ffn',
+    version='0.1.0',
+    author='Michal Januszewski',
+    author_email='mjanusz@google.com',
+    packages=['ffn', 'ffn.inference', 'ffn.training', 'ffn.utils'],
+    scripts=[
+        'build_coordinates.py',
+        'compute_partitions.py',
+        'run_inference.py',
+        'train.py',
+    ],
+    url='https://github.com/google/ffn',
+    license='LICENSE',
+    description='Flood-Filling Networks for volumetric instance segmentation',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'scikit-image',
+        'scipy',
+        'numpy',
+        'tensorflow',
+        'h5py',
+        'pillow',
+        'absl-py',
+    ],
 )
