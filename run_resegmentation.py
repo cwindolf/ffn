d17b67c8a87ae11e0c77f337ba5708ec2c583429
Charlie Windolf <cwindolf95@gmail.com>
2020-03-31 11:04:01 -0400

    Switch from absl-flags to argparse (since we are accumuluting subcommands and not really using absl features.)

--- run_resegmentation.py
+++ run_resegmentation.py
@@ -3,16 +3,19 @@ I recommend running this command inside GNU parallel like:
 
  $ parallel --lb \
    "srun $(SRUNFLAGS) python run_resegmentation.py \
+       run \
        --resegmentation_request XXX \
        --rank {} --nworkers 4" \
    ::: 0 1 2 3
 
 for easy rank-based parallelism.
 """
+import argparse
 import numpy as np
 import os.path
-import logging
+import joblib
 import h5py
+import logging
 
 from google.protobuf import text_format
 from ffn.inference import inference_pb2
@@ -21,54 +24,11 @@ from ffn.inference import resegmentation
 from ffn.inference import resegmentation_analysis
 from ffn.utils import geom_utils
 
-import joblib
-
-from absl import app
-from absl import flags
-
-
-flags.DEFINE_string(
-    "resegmentation_request", "", "Path to ResegmentationRequest proto",
-)
-
-flags.DEFINE_boolean(
-    "analyze_results",
-    False,
-    "Run this script once without this flag to run the required "
-    "resegmentation inferences, then run with this flag set to "
-    "analyze the results of those resegmentations.",
-)
-flags.DEFINE_string(
-    "affinities_npy",
-    "",
-    "If --analyze_results, we will save affinities to this "
-    ".npy path in the neuclease merge table format.",
-)
-
-flags.DEFINE_integer(
-    "nthreads",
-    0,
-    "If set, overrides the `concurrent_requests` field set in the proto."
-    "Special values: 0 says use proto's value, -1 says ncpus - 1, else "
-    "interpreted literally.",
-)
 
 # This is used by resegmentation during EDT to specify
 # anisotropy of the metric. Our voxels are isotropic so
 # not worried about setting this to physical units.
 VOXEL_SZ = [1, 1, 1]
-
-# mpi-style script level parallelism
-flags.DEFINE_integer("rank", 0, "My worker id.")
-flags.DEFINE_integer("nworkers", 1, "Number of workers.")
-
-flags.DEFINE_boolean(
-    "bigmem",
-    False,
-    "Load seg into memory during analysis.",
-)
-
-FLAGS = flags.FLAGS
 
 
 # What's this? See:
@@ -86,30 +46,31 @@ MERGE_TABLE_DTYPE = [
 ]
 
 
-def get_resegmentation_request():
+def get_resegmentation_request(resegmentation_request_path):
     resegmentation_request = inference_pb2.ResegmentationRequest()
-    if FLAGS.resegmentation_request.endswith("txt"):
+    if resegmentation_request_path.endswith("txt"):
         logging.info("Loading resegmentation request from text format...")
-        with open(FLAGS.resegmentation_request, "r") as reseg_req_f:
+        with open(resegmentation_request_path, "r") as reseg_req_f:
             text_format.Parse(reseg_req_f.read(), resegmentation_request)
     else:
         logging.info("Loading resegmentation request from binary format...")
-        with open(FLAGS.resegmentation_request, "rb") as reseg_req_f:
+        with open(resegmentation_request_path, "rb") as reseg_req_f:
             resegmentation_request.ParseFromString(reseg_req_f.read())
     return resegmentation_request
 
 
-def analyze_results():
+def analyze_results(reseg_req_path, affinities_npy, bigmem, nthreads):
     """Produce affinities from completed ResegmentationRequest."""
     # Configure logger
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger("affinities")
 
-    assert bool(FLAGS.affinities_npy)
-    assert not os.path.exists(FLAGS.affinities_npy)
+    assert bool(affinities_npy)
+    assert not os.path.exists(affinities_npy)
+    assert affinities_npy.endswith(".npy")
 
     # Get the ResegmentationRequest
-    resegmentation_request = get_resegmentation_request()
+    resegmentation_request = get_resegmentation_request(reseg_req_path)
     npoints = len(resegmentation_request.points)
 
     # Load up seg volume
@@ -118,7 +79,7 @@ def analyze_results():
         dataset,
     ) = resegmentation_request.inference.init_segmentation.hdf5.split(":")
     init_segmentation = h5py.File(path, "r")[dataset]
-    if FLAGS.bigmem:
+    if bigmem:
         logger.info("Loading segmentation into memory, since you asked.")
         init_segmentation = init_segmentation[:]
 
@@ -151,7 +112,7 @@ def analyze_results():
         return pair_resegmentation_result
 
     results = []
-    nthreads = -1 if FLAGS.nthreads == 0 else FLAGS.nthreads
+    nthreads = -1 if nthreads == 0 else nthreads
     with joblib.Parallel(nthreads, require="sharedmem", verbose=100) as par:
         for result in par(
             joblib.delayed(analyze_point)(i) for i in range(npoints)
@@ -175,32 +136,29 @@ def analyze_results():
         for res in results
     ]
     merge_table = np.array(merge_table, dtype=MERGE_TABLE_DTYPE)
-    np.save(FLAGS.affinities_npy, merge_table)
+    np.save(affinities_npy, merge_table)
 
 
-def do_resegmentation():
+def run_inferences(reseg_req_path, nthreads, rank, nworkers):
     """Run inferences specified in a ResegmentationRequest."""
     # Configure logger
     logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(f"[reseg rank{FLAGS.rank}]")
+    logger = logging.getLogger(f"[reseg rank{rank}]")
 
     # Get the ResegmentationRequest
-    resegmentation_request = get_resegmentation_request()
+    resegmentation_request = get_resegmentation_request(reseg_req_path)
 
     # Figure out this rank's role (might be the only rank.)
-    rank = FLAGS.rank
-    nworkers = FLAGS.nworkers
     num_points_total = len(resegmentation_request.points)
     my_points = list(range(rank, num_points_total, nworkers))
-    nthreads = resegmentation_request.inference.concurrent_requests
-    if FLAGS.nthreads != 0:
-        logging.info(
+    if nthreads == 0:
+        nthreads = resegmentation_request.inference.concurrent_requests
+    else:
+        logger.info(
             "Overriding number of threads set in proto. Was %d, now %d.",
             resegmentation_request.inference.concurrent_requests,
-            FLAGS.nthreads,
+            nthreads,
         )
-        nthreads = FLAGS.nthreads
-
     logger.info("%d points to process on %d ranks", num_points_total, nworkers)
     logger.info(
         "rank %d processing %d points on %d threads",
@@ -237,4 +195,69 @@ def main(unused_argv):
 
 
 if __name__ == "__main__":
-    app.run(main)
+    # ----------------------------- args ------------------------------
+    ap = argparse.ArgumentParser()
+
+    # We'll use a subparser interface to break up the steps.
+    sp = ap.add_subparsers(dest="task")
+
+    # Step 1: Resegmentation request runner ---------------------------
+    run_p = sp.add_parser(
+        "run",
+        help="Step 1: Run the inferences specified in the "
+        "ResegmentationRequest.",
+    )
+    run_p.add_argument(
+        "--resegmentation_request",
+        help="Path to (binary or text) ResegmentationRequest proto.",
+    )
+    run_p.add_argument(
+        "--nthreads",
+        type=int,
+        default=0,
+        help="Number of threads per rank (overrides concurrent_requests field "
+        "in the proto if != 0).",
+    )
+    mpi_g = run_p.add_argument_group(
+        title="Inter-node Parallelism",
+        description="MPI style rank-based multi node parallelism. Work is "
+        "split naively across the ranks.",
+    )
+    mpi_g.add_argument("--rank", type=int, default=0)
+    mpi_g.add_argument("--nworkers", type=int, default=1)
+
+    # Step 2: Analysis / merge table builder --------------------------
+    ana_p = sp.add_parser(
+        "analyze",
+        help="Step 2: Analyze results from run step and spit out merge table.",
+    )
+    ana_p.add_argument(
+        "--resegmentation_request",
+        help="Path to (binary or text) ResegmentationRequest proto.",
+    )
+    ana_p.add_argument(
+        "--affinities_npy",
+        help="Place to save the affinities (aka merge table for neuclease).",
+    )
+    ana_p.add_argument(
+        "--nthreads", help="Number of analysis threads.",
+    )
+    ana_p.add_argument("--bigmem", type=bool, help="Load whole seg into mem.")
+
+    args = ap.parse_args()
+
+    # ------------------------- run the task --------------------------
+    if args.task == "run":
+        run_inferences(
+            args.resegmentation_request,
+            args.nthreads,
+            args.rank,
+            args.nworkers,
+        )
+    elif args.task == "analyze":
+        analyze_results(
+            args.resegmentation_request,
+            args.affinities_npy,
+            args.bigmem,
+            args.nthreads,
+        )
