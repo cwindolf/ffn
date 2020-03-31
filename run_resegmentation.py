e9901ca2efa109df92fd86d018d129988eab0c43
Charlie Windolf <cwindolf95@gmail.com>
2020-03-31 12:45:22 -0400

    Move  into

--- run_resegmentation.py
+++ run_resegmentation.py
@@ -11,19 +11,22 @@ I recommend running this command inside GNU parallel like:
 for easy rank-based parallelism.
 """
 import argparse
-import numpy as np
-import os.path
-import joblib
-import h5py
+import json
 import logging
+import os
 
+import h5py
+import joblib
+import numpy as np
 from google.protobuf import text_format
-from ffn.inference import inference_pb2
+
 from ffn.inference import inference
+from ffn.inference import inference_pb2
 from ffn.inference import resegmentation
 from ffn.inference import resegmentation_analysis
+from ffn.utils import bounding_box_pb2
 from ffn.utils import geom_utils
-
+from ffn.utils import pair_detector
 
 # This is used by resegmentation during EDT to specify
 # anisotropy of the metric. Our voxels are isotropic so
@@ -45,6 +48,8 @@ MERGE_TABLE_DTYPE = [
     ("score", "<f4"),
 ]
 
+# ------------------------------ library ------------------------------
+
 
 def get_resegmentation_request(resegmentation_request_path):
     resegmentation_request = inference_pb2.ResegmentationRequest()
@@ -59,15 +64,173 @@ def get_resegmentation_request(resegmentation_request_path):
     return resegmentation_request
 
 
+# --------------- step 0: build resegmentation request ----------------
+
+
+def build_reseg_req(
+    resegmentation_request_path,
+    inference_request_path,
+    bounding_box_path,
+    init_segmentation_h5,
+    output_directory,
+    subdir_digits,
+    max_retry_iters,
+    segment_recovery_fraction,
+    max_distance,
+    radius,
+    nthreads,
+    bigmem,
+):
+    logger = logging.getLogger("buildrr")
+    # Make sure good ext -- we use ext to decide behavior.
+    assert resegmentation_request_path.split(".")[-1] in ("pb", "pbtxt")
+
+    # Load up inference request
+    inference_request = inference_pb2.InferenceRequest()
+    if inference_request_path:
+        with open(inference_request_path) as inference_request_f:
+            text_format.Parse(inference_request_f.read(), inference_request)
+
+    # Load up bounding box
+    # We compute pair resegmentation points for all neurons
+    # that intersect with the bounding box, but provide no
+    # guarantees about what will happen just outside the boundary.
+    # There might be some points there or there might not.
+    bbox = bounding_box_pb2.BoundingBox()
+    with open(bounding_box_path) as bbox_f:
+        text_format.Parse(bbox_f.read(), bbox)
+
+    # Reseg points ----------------------------------------------------
+    # Build a data structure that will help us quickly check
+    # whether two segments cannot possibly overlap.
+    pd = pair_detector.PairDetector(
+        init_segmentation_h5,
+        bbox,
+        nthreads,
+        max_distance=max_distance,
+        bigmem=bigmem,
+    )
+    logger.info(
+        "Points were in bounding box: %s-%s", pd.min_approach, pd.max_approach,
+    )
+
+    # List points
+    resegmentation_points = []
+    for id_a, id_b, point in pd.pairs_and_points():
+        # Build ResegmentationPoint proto
+        rp = inference_pb2.ResegmentationPoint()
+        rp.id_a = id_a
+        rp.id_b = id_b
+        # PairDetector gets things XYZ ordered.
+        rp.point.x, rp.point.y, rp.point.z = point
+        # OK bai
+        resegmentation_points.append(rp)
+
+    # Build the ResegmentationRequest ---------------------------------
+    logger.info("Building the ResegmentationRequest...")
+
+    # Some fields we set with the constructor...
+    resegmentation_request = inference_pb2.ResegmentationRequest(
+        inference=inference_request,
+        points=resegmentation_points,
+        output_directory=output_directory,
+        subdir_digits=subdir_digits,
+        max_retry_iters=max_retry_iters,
+        segment_recovery_fraction=segment_recovery_fraction,
+    )
+    # Some (nested) fields are easier to just set.
+    # Patch the inference request to point to the initial segmentation
+    resegmentation_request.inference.init_segmentation.hdf5 = (
+        init_segmentation_h5
+    )
+    # Resegmentation and analysis radius
+    resegmentation_request.radius.x = radius
+    resegmentation_request.radius.y = radius
+    resegmentation_request.radius.z = radius
+    # Following suggestion in a comment in ResegmentationRequest proto,
+    # compute analysis radius by subtracting FFN's FOV radius from
+    # the resegmentation radius.
+    model_args = json.loads(inference_request.model_args)
+    ffn_fov_radius = model_args.get("fov_size", [24, 24, 24])
+    resegmentation_request.analysis_radius.x = radius - ffn_fov_radius[0]
+    resegmentation_request.analysis_radius.y = radius - ffn_fov_radius[1]
+    resegmentation_request.analysis_radius.z = radius - ffn_fov_radius[2]
+    # Write request to output file
+    if resegmentation_request_path.endswith(".pbtxt"):
+        # Text output for humans
+        with open(resegmentation_request_path, "w") as out:
+            out.write(text_format.MessageToString(resegmentation_request))
+    elif resegmentation_request_path.endswith(".pb"):
+        # Binary output for robots
+        with open(resegmentation_request_path, "wb") as out:
+            out.write(resegmentation_request.SerializeToString())
+    else:
+        assert False
+    logger.info(f"OK, I wrote {resegmentation_request_path}. Bye...")
+
+
+# --------------- step 1: run resegmentation inferences ---------------
+
+
+def run_inferences(reseg_req_path, nthreads, rank, nworkers):
+    """Run inferences specified in a ResegmentationRequest."""
+    # Configure logger
+    logger = logging.getLogger(f"[reseg rank{rank}]")
+
+    # Get the ResegmentationRequest
+    resegmentation_request = get_resegmentation_request(reseg_req_path)
+
+    # Figure out this rank's role (might be the only rank.)
+    num_points_total = len(resegmentation_request.points)
+    my_points = list(range(rank, num_points_total, nworkers))
+    if nthreads == 0:
+        nthreads = resegmentation_request.inference.concurrent_requests
+    else:
+        logger.info(
+            "Overriding number of threads set in proto. Was %d, now %d.",
+            resegmentation_request.inference.concurrent_requests,
+            nthreads,
+        )
+    logger.info("%d points to process on %d ranks", num_points_total, nworkers)
+    logger.info(
+        "rank %d processing %d points on %d threads",
+        rank,
+        len(my_points),
+        nthreads,
+    )
+
+    # Build run scaffold objects
+    runner = inference.Runner()
+    runner.start(
+        resegmentation_request.inference,
+        executor_expected_clients=len(my_points),
+    )
+
+    # Launch threads
+    logger.info("Starting resegmentation")
+    with joblib.Parallel(nthreads, prefer="threads", verbose=100) as par:
+        for _ in par(
+            joblib.delayed(resegmentation.process_point)(
+                resegmentation_request, runner, i, VOXEL_SZ
+            )
+            for i in my_points
+        ):
+            pass  # no-op to consume generator.
+    logger.info("All done.")
+
+
+# ----- step 2: analyze results from step 1 and make merge table ------
+
+
 def analyze_results(reseg_req_path, affinities_npy, bigmem, nthreads):
     """Produce affinities from completed ResegmentationRequest."""
-    # Configure logger
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("affinities")
-
+    # Crash now rather than after doing all the work
     assert bool(affinities_npy)
     assert not os.path.exists(affinities_npy)
     assert affinities_npy.endswith(".npy")
+
+    # Configure logger
+    logger = logging.getLogger("affinities")
 
     # Get the ResegmentationRequest
     resegmentation_request = get_resegmentation_request(reseg_req_path)
@@ -119,6 +282,7 @@ def analyze_results(reseg_req_path, affinities_npy, bigmem, nthreads):
             results.append(result)
 
     # Build merge table for neuclease
+    logger.info("Done. Building merge table to save to %s", affinities_npy)
     merge_table = [
         (
             res.id_a,
@@ -138,67 +302,64 @@ def analyze_results(reseg_req_path, affinities_npy, bigmem, nthreads):
     np.save(affinities_npy, merge_table)
 
 
-def run_inferences(reseg_req_path, nthreads, rank, nworkers):
-    """Run inferences specified in a ResegmentationRequest."""
-    # Configure logger
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(f"[reseg rank{rank}]")
-
-    # Get the ResegmentationRequest
-    resegmentation_request = get_resegmentation_request(reseg_req_path)
-
-    # Figure out this rank's role (might be the only rank.)
-    num_points_total = len(resegmentation_request.points)
-    my_points = list(range(rank, num_points_total, nworkers))
-    if nthreads == 0:
-        nthreads = resegmentation_request.inference.concurrent_requests
-    else:
-        logger.info(
-            "Overriding number of threads set in proto. Was %d, now %d.",
-            resegmentation_request.inference.concurrent_requests,
-            nthreads,
-        )
-    logger.info("%d points to process on %d ranks", num_points_total, nworkers)
-    logger.info(
-        "rank %d processing %d points on %d threads",
-        rank,
-        len(my_points),
-        nthreads,
-    )
-
-    # Build run scaffold objects
-    runner = inference.Runner()
-    runner.start(
-        resegmentation_request.inference,
-        executor_expected_clients=len(my_points),
-    )
-
-    # Launch threads
-    logger.info("Starting resegmentation")
-    with joblib.Parallel(nthreads, prefer="threads", verbose=100) as par:
-        for _ in par(
-            joblib.delayed(resegmentation.process_point)(
-                resegmentation_request, runner, i, VOXEL_SZ
-            )
-            for i in my_points
-        ):
-            pass
-    logger.info("All done.")
-
-
-def main(unused_argv):
-    if FLAGS.analyze_results:
-        analyze_results()
-    else:
-        do_resegmentation()
-
-
+# ------------------------------- main --------------------------------
 if __name__ == "__main__":
     # ----------------------------- args ------------------------------
     ap = argparse.ArgumentParser()
 
     # We'll use a subparser interface to break up the steps.
     sp = ap.add_subparsers(dest="task")
+
+    # Step 0: Detect pair candidates and build ResegmentationRequest --
+    brr_p = sp.add_parser(
+        "build_req",
+        help="Step 0: Detect possible pairs and make a Resegmentation"
+        "Request proto to pass to later steps.",
+    )
+    brr_p.add_argument(
+        "--resegmentation_request",
+        help="Path to write ResegmentationRequest proto. If ends with .pb, "
+        "a binary proto is written. If ends with .pbtxt, text proto. Binary "
+        "is much faster to load later, text is just there for debugging.",
+    )
+    brr_p.add_argument(
+        "--inference_request",
+        help="Path to the original InferenceRequest proto. The Resegmentation"
+        "Request proto that this script generates will be based on it.",
+    )
+    brr_p.add_argument(
+        "--bounding_box",
+        help="Path to a BoundingBox proto. We will look for reseg pairs in "
+        "this bbox.",
+    )
+    brr_p.add_argument(
+        "--init_segmentation",
+        help="<hdf5 path>:<dset> pointing to the initial consensus "
+        "segmentation (result of run_consensus.py after run_inference.py).",
+    )
+    brr_p.add_argument(
+        "--nthreads",
+        type=int,
+        default=-1,
+        help="Number of threads to use in pair detector.",
+    )
+    brr_p.add_argument(
+        "--bigmem",
+        action="store_true",
+        help="Load init segmentation into memory when detecting pairs.",
+    )
+    rr_g = brr_p.add_argument_group(
+        title="ResegmentationRequest proto fields",
+        description="These flags will set fields in the output proto. Check "
+        "out inference.proto to see their documentation, and feel free to add "
+        "more of them here.",
+    )
+    rr_g.add_argument("--output_directory")
+    rr_g.add_argument("--subdir_digits", type=int, default=2)
+    rr_g.add_argument("--max_retry_iters", type=int, default=1)
+    rr_g.add_argument("--segment_recovery_fraction", type=float, default=0.5)
+    rr_g.add_argument("--max_distance", type=float, default=2.0)
+    rr_g.add_argument("--radius", type=int, default=48)
 
     # Step 1: Resegmentation request runner ---------------------------
     run_p = sp.add_parser(
@@ -241,12 +402,34 @@ if __name__ == "__main__":
     ana_p.add_argument(
         "--nthreads", help="Number of analysis threads.",
     )
-    ana_p.add_argument("--bigmem", type=bool, help="Load whole seg into mem.")
+    ana_p.add_argument(
+        "--bigmem", action="store_true", help="Load whole seg into mem."
+    )
 
     args = ap.parse_args()
 
     # ------------------------- run the task --------------------------
-    if args.task == "run":
+    if "DEBUG" in os.environ:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.task == "build_req":
+        build_reseg_req(
+            args.resegmentation_request,
+            args.inference_requset,
+            args.bounding_box,
+            args.init_segmentation,
+            args.output_directory,
+            args.subdir_digits,
+            args.max_retry_iters,
+            args.segment_recovery_fraction,
+            args.max_distance,
+            args.radius,
+            args.nthreads,
+            args.bigmem,
+        )
+    elif args.task == "run":
         run_inferences(
             args.resegmentation_request,
             args.nthreads,
