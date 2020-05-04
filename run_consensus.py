c73682b9c7eb0d4c0593cf9524fc1d5705a25cca
Charlie Windolf <cwindolf95@gmail.com>
2020-05-03 21:23:57 -0400

    Debug id logic

--- run_consensus.py
+++ run_consensus.py
@@ -144,6 +144,7 @@ def merge_from_min_id(out, seg, mask, min_new_id, scratch=None, tag=""):
         assert scratch.size == seg.size
         scratch.fill(0)
     else:
+        print("new scratch")
         scratch = np.zeros(out.size, dtype=np.uint32)
     scratch[mask] = seg[mask]
     # Split CCs
@@ -195,11 +196,11 @@ def seg_meet(a, b, min_new_id=1):
     out = np.zeros(orig_shape, dtype=np.uint32)
     scratch = np.empty(a.shape, dtype=np.uint32)
     # Perform the merges
-    new_max_id = merge_from_min_id(out, a, a_not_b, min_new_id, scratch=scratch,
+    new_max_id = merge_from_min_id(out, a, a_not_b, min_new_id, #scratch=scratch,
                                    tag=f"a_not_b prev mi={min_new_id}")
-    new_max_id = merge_from_min_id(out, b, b_not_a, new_max_id + 1, scratch=scratch,
+    new_max_id = merge_from_min_id(out, b, b_not_a, new_max_id + 1, #scratch=scratch,
                                    tag=f"b_not_a prev mi={new_max_id}")
-    new_max_id = merge_from_min_id(out, a, b_and_a, new_max_id + 1, scratch=scratch,
+    new_max_id = merge_from_min_id(out, a, b_and_a, new_max_id + 1, #scratch=scratch,
                                    tag=f"both prev mi={new_max_id}")
     assert new_max_id < np.iinfo(np.uint32).max
 
