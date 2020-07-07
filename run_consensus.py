534e307ee1dbf660a740da0eb6c64c4a0db64b4a
Charlie Windolf <cwindolf95@gmail.com>
2020-07-07 11:47:11 -0400

    Path problem.

--- run_consensus.py
+++ run_consensus.py
@@ -194,9 +194,15 @@ def seg_meet(a, b, min_new_id=1):
     out = np.zeros(orig_shape, dtype=np.uint32)
     scratch = np.empty(a.shape, dtype=np.uint32)
     # Perform the merges
-    new_max_id = merge_from_min_id(out, a, a_not_b, min_new_id, scratch=scratch)
-    new_max_id = merge_from_min_id(out, b, b_not_a, new_max_id + 1, scratch=scratch)
-    new_max_id = merge_from_min_id(out, a, b_and_a, new_max_id + 1, scratch=scratch)
+    new_max_id = merge_from_min_id(
+        out, a, a_not_b, min_new_id, scratch=scratch
+    )
+    new_max_id = merge_from_min_id(
+        out, b, b_not_a, new_max_id + 1, scratch=scratch
+    )
+    new_max_id = merge_from_min_id(
+        out, a, b_and_a, new_max_id + 1, scratch=scratch
+    )
     assert new_max_id < np.iinfo(np.uint32).max
 
     logging.info(f"seg_meet max id {new_max_id}")
