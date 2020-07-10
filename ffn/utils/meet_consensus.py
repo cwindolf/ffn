4f112b7648e115a63db4e7b5ca0291458efa3e05
Charlie Windolf <cwindolf95@gmail.com>
2020-07-10 12:02:14 -0400

    Weird...

--- ffn/utils/meet_consensus.py
+++ ffn/utils/meet_consensus.py
@@ -266,6 +266,7 @@ def load_and_compute_meet(segmentation_dirs, subvolume=None, min_ffn_size=0):
     """
     # location of segs to load
     load_corner = (0, 0, 0)
+    logging.info(f"{subvolume}")
     if subvolume is not None:
         load_corner = subvolume.start
 
