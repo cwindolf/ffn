55afb9fb0cc6061306f8a4eb67296e04818e9962
Charlie Windolf <cwindolf95@gmail.com>
2020-07-15 10:53:30 -0400

    Threaded

--- show_simple_seg_stats.py
+++ show_simple_seg_stats.py
@@ -1,4 +1,5 @@
 import argparse
+from multiprocessing.pool import ThreadPool
 
 from ffn.utils import datspec as dx
 from ffn.utils import unsupervised_metrics
@@ -10,9 +11,15 @@ ap.add_argument("datspecs", nargs="+")
 
 args = ap.parse_args()
 
-for datspec in args.datspecs:
-    print(datspec)
+
+def stats_thread(datspec):
     seg = dx.loadspec(datspec)
     metrics = unsupervised_metrics.unsupervised_metrics(seg)
-    for name, met in zip(metrics._fields, metrics):
-        print(f" - {name}: {met}")
+    return datspec, metrics
+
+
+with ThreadPool() as pool:
+    for datspec, metrics in pool.imap(stats_thread, args.datspecs):
+        print(datspec)
+        for name, met in zip(metrics._fields, metrics):
+            print(f" - {name}: {met}")
